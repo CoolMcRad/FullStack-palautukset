d008c08f877769cf1e548ef7d6850b376ddb3869
CoolMcRad <santtu-12@hotmail.com>
2020-01-27 18:44:03 +0200

    Tehtävien 1.12-1.14 projektin commit

--- Osa1/README.md
+++ Osa1/README.md
@@ -1,2 +1,3 @@
 Kurssitiedot kansiossa projekti johon tehty tehtävät 1.1-1.5
 unicafe kansiossa projekti johon tehty tehtävät 1.6-1.11
+anekdootit kansiossa projekti johon tehty tehtävät 1.12-1.14
---
+++ Osa1/anekdootit/src/index.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const Display = ({anekdootti, votes}) => (
+  <div>
+    <p>{anekdootti}</p>
+    <p>has {votes} votes</p>
+  </div>
+)
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(6).fill(0))
+  const [mostVotes, setMostVotes] = useState(0)
+
+  const handleClick = () => 
+    setSelected(Math.floor(Math.random() * 6))
+
+  const handleClickVote = () => {
+    const t = [...votes]
+    t[selected] += 1
+    if (t[selected] > t[mostVotes]) {
+      setMostVotes(selected)
+    }
+    setVotes(t)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the Day !</h1>
+        <Display anekdootti={props.anecdotes[selected]} votes={votes[selected]}></Display>
+        <Button onClick={handleClick} text='Random anecdote' />
+        <Button onClick={handleClickVote} text='Vote' />
+      <h1>Anecdote With Most Votes !</h1>
+        <Display anekdootti={props.anecdotes[mostVotes]} votes={votes[mostVotes]}></Display>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
